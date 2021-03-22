020d0a88bdce39967c65f10539e00f8650f95c38
kapil <kapil@phoenixcoded.co>
2021-03-22 13:10:50 +0100

    change src and destination folder in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -32,13 +32,13 @@ const layout = {
 //  [ scss compiler ] start
 gulp.task('sass', function() {
     // main style css
-    return gulp.src('src/assets/scss/*.scss')
+    return gulp.src('resources/assets/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(cssbeautify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(gulp.dest('public/assets/css'))
 })
 //  [ scss compiler ] end
 
@@ -174,14 +174,14 @@ gulp.task('build', function() {
     npmlodash(required_libs).forEach(function(assets, type) {
         if (type == "css") {
             gulp.src(assets)
-                .pipe(gulp.dest("dist/assets/css/plugins"));
+                .pipe(gulp.dest("public/assets/css/plugins"));
         } else {
             gulp.src(assets)
-                .pipe(gulp.dest("dist/assets/js/plugins"));
+                .pipe(gulp.dest("public/assets/js/plugins"));
         }
     });
-    var cpyassets = gulp.src(['src/assets/**/*.*', '!src/assets/scss/**/*.*'])
-        .pipe(gulp.dest('dist/assets'));
+    var cpyassets = gulp.src(['resources/assets/**/*.*', '!resources/assets/scss/**/*.*'])
+        .pipe(gulp.dest('public/assets'));
     return merge(cpyassets);
 });
 
@@ -189,24 +189,24 @@ gulp.task('build', function() {
 
 //  [ build html ] start
 gulp.task('build-html', function() {
-    return gulp.src('src/html/*.html')
+    return gulp.src('resources/views/html/*.html')
         .pipe(fileinclude({
             context: layout,
             prefix: '@@',
             basepath: '@file',
             indent: true
         }))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest("public"))
 })
 //  [ build html ] end
 
 //  [ build js ] start
 gulp.task('build-js', function() {
-    var layoutjs = gulp.src('src/assets/js/*.js')
-        .pipe(gulp.dest('dist/assets/js'))
+    var layoutjs = gulp.src('resources/assets/js/*.js')
+        .pipe(gulp.dest('public/assets/js'))
 
-    var pagesjs = gulp.src('src/assets/js/pages/*.js')
-        .pipe(gulp.dest('dist/assets/js/pages'))
+    var pagesjs = gulp.src('resources/assets/js/pages/*.js')
+        .pipe(gulp.dest('public/assets/js/pages'))
 
     return merge(layoutjs, pagesjs);
 })
@@ -215,26 +215,26 @@ gulp.task('build-js', function() {
 //  [ scss compiler ] start
 gulp.task('mincss', function() {
     // main style css
-    return gulp.src('src/assets/scss/*.scss')
+    return gulp.src('resources/assets/scss/*.scss')
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(cssbeautify())
-        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(gulp.dest('public/assets/css'))
         .pipe(cssmin())
-        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(gulp.dest('public/assets/css'))
 })
 //  [ scss compiler ] end
 
 //  [ uglify js ] start
 gulp.task('uglify', function() {
-    var layoutjs = gulp.src('src/assets/js/*.js')
+    var layoutjs = gulp.src('resources/assets/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(gulp.dest('public/assets/js'))
 
-    var pagesjs = gulp.src('src/assets/js/pages/*.js')
+    var pagesjs = gulp.src('resources/assets/js/pages/*.js')
         .pipe(babel())
         .pipe(uglify())
-        .pipe(gulp.dest('dist/assets/js/pages'))
+        .pipe(gulp.dest('public/assets/js/pages'))
 
     return merge(layoutjs, pagesjs);
 })
@@ -242,7 +242,7 @@ gulp.task('uglify', function() {
 
 //  [ minify html ] start
 gulp.task('htmlmin', function() {
-    return gulp.src('src/html/*.html')
+    return gulp.src('resources/views/html/*.html')
         .pipe(fileinclude({
             context: layout,
             prefix: '@@',
@@ -252,32 +252,32 @@ gulp.task('htmlmin', function() {
         .pipe(htmlmin({
             collapseWhitespace: true
         }))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest("public"))
 })
 //  [ minify html ] end
 
 //  [ image optimizer ] start
 gulp.task('imgmin', function() {
-    return gulp.src('src/assets/img/**/*.{jpg,png}')
+    return gulp.src('resources/assets/img/**/*.{jpg,png}')
         .pipe(smushit())
-        .pipe(gulp.dest('dist/assets/img'));
+        .pipe(gulp.dest('public/assets/img'));
 });
 //  [ image optimizer ] end
 
 //  [ browser reload ] start
 gulp.task("browserSync", function() {
     browsersync.init({
-        server: "dist/"
+        server: "public/"
     });
 });
 //  [ browser reload ] end
 
 //  [ watch ] start
 gulp.task('watch', function() {
-    gulp.watch('src/assets/scss/**/*.scss', gulp.series('sass')).on('change', browsersync.reload);
-    gulp.watch('src/assets/js/**/*.js', gulp.series('build-js')).on('change', browsersync.reload);
-    gulp.watch('src/html/**/*.html', gulp.series('build-html')).on('change', browsersync.reload);
-    gulp.watch('src/doc/**/*.html', gulp.series('build')).on('change', browsersync.reload);
+    gulp.watch('resources/assets/scss/**/*.scss', gulp.series('sass')).on('change', browsersync.reload);
+    gulp.watch('resources/assets/js/**/*.js', gulp.series('build-js')).on('change', browsersync.reload);
+    gulp.watch('resources/views/html/**/*.html', gulp.series('build-html')).on('change', browsersync.reload);
+    gulp.watch('resources/views/doc/**/*.html', gulp.series('build')).on('change', browsersync.reload);
 })
 //  [ watch ] start
 const compile = gulp.parallel('browserSync', 'watch');
@@ -297,9 +297,9 @@ gulp.task('default',
 
 //  [ watch minify ] start
 gulp.task('watch-minify', function() {
-    gulp.watch('src/assets/scss/**/*.scss', gulp.series('mincss'));
-    gulp.watch('src/assets/js/**/*.js', gulp.series('uglify'));
-    gulp.watch('src/html/**/*.html', gulp.series('htmlmin'));
-    gulp.watch('src/doc/**/*.html', gulp.series('build'));
+    gulp.watch('resources/assets/scss/**/*.scss', gulp.series('mincss'));
+    gulp.watch('resources/assets/js/**/*.js', gulp.series('uglify'));
+    gulp.watch('resources/views/html/**/*.html', gulp.series('htmlmin'));
+    gulp.watch('resources/views/**/*.html', gulp.series('build'));
 })
 //  [ watch minify ] start
